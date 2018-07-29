a28d04c6406091880fd2a6e5c7c61c74cf386589
Ganioc Yang <ganioc.yang@gmail.com>
2018-07-29 10:16:23 +0800

    add cppstudy

---
+++ cpp/testcpp.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "testcpp running" << endl;
+
+    string s1;
+    string s2("value");
+    string s3(5, '-');
+
+    s1 = "hello world";
+
+    cout << s1 << endl;
+    cout << s2 << endl;
+    cout << s3 << endl;
+
+    return 0;
+}
