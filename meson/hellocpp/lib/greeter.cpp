7ea218355af6a9d1126ef6c01f30dd27614f5f0d
yango <o3.yang@hotmail.com>
2023-05-06 11:54:31 +0800

    add meson dir

---
+++ meson/hellocpp/lib/greeter.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+#include "greeter.h"
+
+void print_greeting(){
+    std::cout << "Greeting ..." << std::endl;
+}
---
+++ meson/hellocpp/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <greeter.h>
+
+using namespace std;
+
+int main(){
+    cout << "main()" << endl;
+
+    print_greeting();
+
+    return 0;
+}
