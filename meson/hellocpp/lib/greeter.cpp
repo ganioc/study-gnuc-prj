0a88db58757016200223fa98073449b295820feb
yango <o3.yang@hotmail.com>
2023-05-06 14:51:14 +0800

    update meson

--- meson/hellocpp/lib/greeter.cpp
+++ meson/hellocpp/lib/greeter.cpp
@@ -3,5 +3,20 @@
 #include "greeter.h"
 
 void print_greeting(){
+    TRACE();
     std::cout << "Greeting ..." << std::endl;
+    TRACE();
+}
+
+void print_trace(const char *function_name,
+    const char *file_name,
+    const int line_num)
+{
+    std::cout << "TRACE: "
+            << function_name
+            << ' '
+            << file_name
+            << " : "
+            << line_num
+            << '\n';
 }
--- meson/hellocpp/lib/greeter.h
+++ meson/hellocpp/lib/greeter.h
@@ -1,3 +1,14 @@
 #pragma once
 
 void print_greeting();
+void print_trace(const char *function_name,
+    const char *file_name,
+    const int line_num);
+
+#ifdef GREETING_TRACE
+#define TRACE() print_trace(__PRETTY_FUNCTION__, \
+    __FILE__, __LINE__)
+#else
+#define TRACE()
+
+#endif
